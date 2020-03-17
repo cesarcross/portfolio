9b462506048e57405e26aa961537ac57a688b9af
Cesar Corregiari <cesarcorregiari@gmail.com>
2020-03-17 14:55:03 -0300

    /ofertas going on

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'ofertas', to: 'pages#ofertas', as: :ofertas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :pages
 end
