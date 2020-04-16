de2aece1e7b34ad3cd78de578deb2c8bdd0b966d
Cesar Corregiari <cesarcorregiari@gmail.com>
2020-04-16 14:49:24 -0300

    hotmart test

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'ofertas', to: 'pages#ofertas', as: :ofertas
+  get 'timao', to: 'pages#timao', as: :timao
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :pages, only: %i[ofertas create]
 end
